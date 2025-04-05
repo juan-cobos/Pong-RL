2dc40d3a38d7d3f7b892fc3ee93b45b0d980ec51
Juan Cobos <juan.cobos@igf.cnrs.fr>
2025-04-05 20:45:27 +0200

    Added resizability and fixed ball-paddle glitches

--- pong.py
+++ pong.py
@@ -1,6 +1,9 @@
 import numpy as np
 import random
 import pygame
+from pygame import VIDEORESIZE
+random.seed(55)
+np.random.seed(45)
 
 ACTIONS = {
     0: np.array([0, 1], dtype=np.int8),
@@ -15,24 +18,19 @@ COLOR_TO_RGB = {
 }
 
 COLOR_TO_GRAY = {
-    "black"     : (0, 0, 0),
-    "dark_gray"  : (50, 50, 50),
-    "mid_gray"  : (128, 128, 128),
-    "light_gray": (200, 200, 200),
-    "white"     : (255, 255, 255),
+    "black"     : 0,
+    "mid_gray"  : 128,
+    "white"     : 255,
 }
 
-WIDTH = 256
-HEIGHT = 256
+WIDTH = 64
+HEIGHT = 64
+
 
 class Paddle:
-    def __init__(self, x, y, width=10, height=30, speed=10, color="red"):
+    def __init__(self, x, y, width, height, speed=3, color="red"):
 
-        #self.shape = width, height
-        # TODO: self.pos has to be a 2D with all the positions filled by the Paddle
-        # [x: , y: ] * length [0, i] cause it's vertical so j will be the same
         # pos = X, Y coordinates from meshgrid: https://numpy.org/doc/2.2/reference/generated/numpy.meshgrid.html
-
         self.X, self.Y = np.meshgrid(range(x-width//2, x+width//2), range(y-height//2, y+height//2))
         self._init_X, self._init_Y = self.X, self.Y
         self.width = width
@@ -40,16 +38,19 @@ class Paddle:
         self.speed = speed
         self.color = color
 
-    def update(self, action):
+    def move(self, action):
         next_Y = self.Y + self.speed * action[1]
-        if np.all(0 <= next_Y) and np.all(next_Y < HEIGHT):
-            self.Y = self.Y + self.speed * action[1] # Pad only updates in Y axis
+        # Move only if it's in grid range
+        if np.all(next_Y >= 0) and np.all(next_Y < HEIGHT):
+            self.Y = next_Y
+        #return self.X, self.Y
+
 
     def reset(self):
         self.X, self.Y = self._init_X, self._init_Y
 
 class Ball:
-    def __init__(self, x, y, radius, speed=5, color="yellow"):
+    def __init__(self, x, y, radius, speed=3, color="yellow"):
         self._init_center = np.array((x, y))
         self.center = np.array((x, y))
         self.radius = radius
@@ -64,10 +65,11 @@ class Ball:
         self.center = self._init_center
         angle = np.radians(random.uniform(-45, 45))
         self.dir = np.array((np.cos(angle), np.sin(angle))) * random.choice([-1, 1])
-
-    def update(self):
-        self.center = self.center + np.int16(self.speed * self.dir)
         self.X, self.Y = self.get_circle()
+
+    def move(self):
+        self.center = self.center + np.int16(self.speed * self.dir)
+        return self.get_circle()
 
     def get_circle(self): # Get the xx, yy coordinates of a circle representation
         xx, yy = np.meshgrid(
@@ -81,15 +83,22 @@ class Ball:
         return x_coords, y_coords # self.X, self.Y
 
 class Pong:
-    def __init__(self, paddle_width=None, paddle_height=None, ball_radius=10):
+    def __init__(self, paddle_width=2, paddle_height=11, ball_radius=2, render_mode="rgb", level="low"):
+
+        render_modes = ("rgb", "grayscale")
+        assert render_mode in render_modes, "Render mode not supported"
+        # TODO: allow for grayscale img to implement only [width, height] array
+
         self.state = np.zeros((WIDTH, HEIGHT, 3), dtype=np.int8)
-        self.paddle1 = Paddle(10, HEIGHT//2)
-        self.paddle2 = Paddle(WIDTH-10, HEIGHT//2)
+        self.paddle1 = Paddle(paddle_width*2, HEIGHT//2, paddle_width, paddle_height)
+        self.paddle2 = Paddle(WIDTH - paddle_width*2, HEIGHT//2, paddle_width, paddle_height)
         self.ball = Ball(WIDTH//2, HEIGHT//2, radius=ball_radius)
 
+        self.bot = PongPolicy(level) # TODO: select policy based on level
         self.window = None
         self.score = [0, 0]
 
+        self.steps = 0
     def update_state(self):
         arr = np.zeros((WIDTH, HEIGHT, 3), dtype=np.uint8)
         arr[:, :] = COLOR_TO_RGB["black"]  # Init all grid black
@@ -99,45 +108,68 @@ class Pong:
         arr[self.paddle2.X, self.paddle2.Y] =  COLOR_TO_RGB[self.paddle2.color]
         arr[self.ball.X, self.ball.Y] =  COLOR_TO_RGB[self.ball.color]
 
-        self.state = arr
+        return arr
 
-    def display_objects(self):
-        # Draw ball
-        pygame.draw.circle(self.window, self.ball.color, self.ball.center, self.ball.radius)
-
+    def display_objects(self, scale_x, scale_y):
         # Draw Paddle1
-        paddle1_rect = pygame.Rect(self.paddle1.X[0][0], self.paddle1.Y[0][0], self.paddle1.width, self.paddle1.height)
+        paddle1_rect = pygame.Rect(
+            self.paddle1.X[0][0] * scale_x,
+            self.paddle1.Y[0][0] * scale_y,
+            self.paddle1.width * scale_x,
+            self.paddle1.height * scale_y
+        )
         pygame.draw.rect(self.window, self.paddle1.color, paddle1_rect)
 
         # Draw Paddle2
-        paddle2_rect = pygame.Rect(self.paddle2.X[0][0], self.paddle2.Y[0][0], self.paddle2.width, self.paddle2.height)
+        paddle2_rect = pygame.Rect(
+            self.paddle2.X[0][0] * scale_x,
+            self.paddle2.Y[0][0] * scale_y,
+            self.paddle2.width  * scale_x,
+            self.paddle2.height * scale_y
+        )
         pygame.draw.rect(self.window, self.paddle2.color, paddle2_rect)
+
+        # Draw ball
+        pygame.draw.circle(
+            self.window,
+            self.ball.color,
+            self.ball.center * np.array([scale_x, scale_y]),
+            self.ball.radius * min(scale_x, scale_y)
+        )
 
     def step(self, action):
         reward = 0
         done = False
+        self.steps += 1
 
-        self.paddle1.update(ACTIONS[action])
-        self.paddle2.update(ACTIONS[action]) # TODO: Describe policy for Paddle2 (Bot)
-        self.ball.update()
+        self.paddle1.move(ACTIONS[action])
+        self.paddle2.move(self.bot.get_action(self.ball, self.paddle2))
+        self.ball.X, self.ball.Y = self.ball.move()
 
-        print("Ball array", self.ball.X)
-        if np.any(self.ball.X < 0) or np.any(self.ball.X > WIDTH): # It means Goal
-            reward = -1 if np.any(self.ball.X < 0) else 1
-            self.score += np.array([0, 1] if np.any(self.ball.X < 0) else [1, 0]) # Update score
-            self.ball.reset(), self.paddle1.reset(), self.paddle2.reset() # Reset to init positions
+        # If it's goal in any side
+        if np.any(self.ball.X <= 0) or np.any(self.ball.X >= WIDTH - 1):
+            reward = -1 if np.any(self.ball.X <= 0) else 1
+            self.score += np.array([0, 1] if np.any(self.ball.X <= 0) else [1, 0])  # Update score
+            self.ball.reset(), self.paddle1.reset(), self.paddle2.reset()  # Reset to init positions
+            self.state = self.update_state()
             return self.state, reward, True
 
-        if (
-                np.any(self.ball.X <= 0) or np.any(self.ball.X >= HEIGHT)
-                or np.any(self.ball.Y <= 0) or np.any(self.ball.Y >= HEIGHT)# Outside the grid
-                or np.any(np.isin(self.ball.X, self.paddle1.X)) and np.any(np.isin(self.ball.Y, self.paddle1.Y)) # Hits paddle1
-                or np.any(np.isin(self.ball.X, self.paddle2.X)) and np.any(np.isin(self.ball.Y, self.paddle2.Y)) # Hits paddle2
-        ):
-            print("Entra")
-            self.ball.dir = -self.ball.dir # Reflect direction
+        hit_walls = np.any(self.ball.Y <= 0) or np.any(self.ball.Y >= HEIGHT - 1)  # Hits side walls
+        hit_paddle1 = ( # Hits paddle1
+                np.any(np.isin(self.ball.X, self.paddle1.X))
+                and np.any(np.isin(self.ball.Y, self.paddle1.Y))
+                and self.ball.dir[0] < 0  # Goes in the right direction
+        )
+        hit_paddle2 = ( # Hits paddle2
+                np.any(np.isin(self.ball.X, self.paddle2.X))
+                and np.any(np.isin(self.ball.Y, self.paddle2.Y))
+                and self.ball.dir[0] > 0  # Goes in the right direction
+        )
 
-        self.update_state() # Update state with the new object locations
+        if hit_walls or hit_paddle1 or hit_paddle2:
+            self.ball.dir = -self.ball.dir  # Reflect direction
+        # Update state with the new object locations
+        self.state = self.update_state()
         self.render()
         return self.state, reward, done
 
@@ -152,22 +184,48 @@ class Pong:
             self.window = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
             pygame.display.set_caption("Pong")
 
-        # TODO: Handle resizable window, whether by a scaling factor on the objects or .transform window ish
-        font = pygame.font.Font('freesansbold.ttf', 20)  # Use default font, size 36
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.reset()
+            #if event.type == pygame.VIDEORESIZE:
+             #   self.window = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
+
+        window_width, window_height = self.window.get_size()
+        scale_x = window_width / WIDTH
+        scale_y = window_height / HEIGHT
+
+        font = pygame.font.Font('freesansbold.ttf', int(2*scale_x)) # Use default font, size 20
         clock = pygame.time.Clock()
-        background = pygame.Surface((WIDTH, HEIGHT))
+        background = pygame.Surface((WIDTH * scale_x, HEIGHT * scale_y))
         background.fill(COLOR_TO_RGB["black"])
 
         # Clear screen
         self.window.blit(background, (0, 0))
         # Display objects
-        self.display_objects()
+        self.display_objects(scale_x, scale_y)
         # Render score
         score_text = font.render(f"Score: {self.score[0]} - {self.score[1]}", True, COLOR_TO_RGB["white"])
-        self.window.blit(score_text, (20, 20))  # Draw score at top-left
+        score_pos = (20 * scale_x, 20 * scale_y)
+        self.window.blit(score_text, score_pos)  # Draw score at top-left
 
         pygame.display.flip()
         clock.tick(30)
 
+class PongPolicy:
+    def __init__(self, level):
+        self.level = level
+
+    def get_action(self, ball: Ball, paddle: Paddle):
+        paddle_y = np.max(paddle.Y) - paddle.height/2
+        dead_zone = 8
+
+        if ball.center[1] > paddle_y + dead_zone:
+            return ACTIONS[0]
+        elif ball.center[1] < paddle_y - dead_zone:
+            return ACTIONS[1]
+        else:
+            return np.array([0, 0])
+
+        "Decides an action based on the ball's and paddle's position."
 
 

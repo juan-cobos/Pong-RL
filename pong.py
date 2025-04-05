bf2127cff8d089889122b4ad5496d7c7df04ca9d
Juan Cobos <juan.cobos@igf.cnrs.fr>
2025-04-05 11:41:59 +0200

    Game working although lacks resizability

--- pong.py
+++ pong.py
@@ -1,17 +1,8 @@
 import numpy as np
-from enum import IntEnum
 import random
+import pygame
 
-from matplotlib.pyplot import xscale
-
-
-class Objects(IntEnum):
-    empty = 0
-    wall = 1
-    paddle = 2
-    ball = 3
-
-actions = {
+ACTIONS = {
     0: np.array([0, 1], dtype=np.int8),
     1 : np.array([0, -1], dtype=np.int8)
 }
@@ -31,119 +22,152 @@ COLOR_TO_GRAY = {
     "white"     : (255, 255, 255),
 }
 
+WIDTH = 256
+HEIGHT = 256
+
 class Paddle:
-    def __init__(self, x, y, width=10, height=30):
+    def __init__(self, x, y, width=10, height=30, speed=10, color="red"):
 
         #self.shape = width, height
         # TODO: self.pos has to be a 2D with all the positions filled by the Paddle
         # [x: , y: ] * length [0, i] cause it's vertical so j will be the same
         # pos = X, Y coordinates from meshgrid: https://numpy.org/doc/2.2/reference/generated/numpy.meshgrid.html
-        self.X, self.Y = np.meshgrid(range(x-width//2, x+width//2), range(y-height//2, y+height//2), indexing="ij")
-        self.color = "red"
 
-class Ball:
-    def __init__(self, x, y, size = 10):
-        self.X, self.Y = np.meshgrid(range(x-size//2, x+size//2), range(y-size//2, y+size//2), indexing="ij")
-        self.dir_space = np.array([[x, y] for y in range(-1, 2) for x in range(-1, 2) if x!=0 or y!=0]) # dir R^2 -> {-1, 0, 1} X {-1, 0, 1} / {0, 0}
-        self.dir = np.array(random.choice(self.dir_space))
-        self.color = "yellow"
-
-class Pong:
-    def __init__(self, width, height, paddle_width=None, paddle_height=None, ball_size=None):
+        self.X, self.Y = np.meshgrid(range(x-width//2, x+width//2), range(y-height//2, y+height//2))
+        self._init_X, self._init_Y = self.X, self.Y
         self.width = width
         self.height = height
-        self.state = np.zeros((width, height), dtype=np.int8)
-        self.paddle1 = Paddle(10, height//2)
-        self.paddle2 = Paddle(width-10, height//2)
-        self.ball = Ball(width//2, height//2, )
+        self.speed = speed
+        self.color = color
+
+    def update(self, action):
+        next_Y = self.Y + self.speed * action[1]
+        if np.all(0 <= next_Y) and np.all(next_Y < HEIGHT):
+            self.Y = self.Y + self.speed * action[1] # Pad only updates in Y axis
+
+    def reset(self):
+        self.X, self.Y = self._init_X, self._init_Y
+
+class Ball:
+    def __init__(self, x, y, radius, speed=5, color="yellow"):
+        self._init_center = np.array((x, y))
+        self.center = np.array((x, y))
+        self.radius = radius
+        self.X, self.Y = self.get_circle()
+
+        angle = np.radians(random.uniform(-45, 45))
+        self.dir = np.array((np.cos(angle), np.sin(angle))) * random.choice([-1, 1])
+        self.speed = speed
+        self.color = color
+
+    def reset(self):
+        self.center = self._init_center
+        angle = np.radians(random.uniform(-45, 45))
+        self.dir = np.array((np.cos(angle), np.sin(angle))) * random.choice([-1, 1])
+
+    def update(self):
+        self.center = self.center + np.int16(self.speed * self.dir)
+        self.X, self.Y = self.get_circle()
+
+    def get_circle(self): # Get the xx, yy coordinates of a circle representation
+        xx, yy = np.meshgrid(
+            range(self.center[0] - self.radius, self.center[0] + self.radius + 1),
+            range(self.center[1] - self.radius, self.center[1] + self.radius + 1)
+        )
+        circle_mask = (xx - self.center[0]) ** 2 + (yy - self.center[1]) ** 2 <= self.radius ** 2
+        yy_idx, xx_idx = np.where(circle_mask)
+        x_coords = xx[yy_idx, xx_idx]
+        y_coords = yy[yy_idx, xx_idx]
+        return x_coords, y_coords # self.X, self.Y
+
+class Pong:
+    def __init__(self, paddle_width=None, paddle_height=None, ball_radius=10):
+        self.state = np.zeros((WIDTH, HEIGHT, 3), dtype=np.int8)
+        self.paddle1 = Paddle(10, HEIGHT//2)
+        self.paddle2 = Paddle(WIDTH-10, HEIGHT//2)
+        self.ball = Ball(WIDTH//2, HEIGHT//2, radius=ball_radius)
+
+        self.window = None
+        self.score = [0, 0]
+
+    def update_state(self):
+        arr = np.zeros((WIDTH, HEIGHT, 3), dtype=np.uint8)
+        arr[:, :] = COLOR_TO_RGB["black"]  # Init all grid black
+
+        # Fill array with objects at their place
+        arr[self.paddle1.X, self.paddle1.Y] =  COLOR_TO_RGB[self.paddle1.color]
+        arr[self.paddle2.X, self.paddle2.Y] =  COLOR_TO_RGB[self.paddle2.color]
+        arr[self.ball.X, self.ball.Y] =  COLOR_TO_RGB[self.ball.color]
+
+        self.state = arr
+
+    def display_objects(self):
+        # Draw ball
+        pygame.draw.circle(self.window, self.ball.color, self.ball.center, self.ball.radius)
+
+        # Draw Paddle1
+        paddle1_rect = pygame.Rect(self.paddle1.X[0][0], self.paddle1.Y[0][0], self.paddle1.width, self.paddle1.height)
+        pygame.draw.rect(self.window, self.paddle1.color, paddle1_rect)
+
+        # Draw Paddle2
+        paddle2_rect = pygame.Rect(self.paddle2.X[0][0], self.paddle2.Y[0][0], self.paddle2.width, self.paddle2.height)
+        pygame.draw.rect(self.window, self.paddle2.color, paddle2_rect)
 
     def step(self, action):
         reward = 0
         done = False
 
-        def in_grid(X, Y): # : 2D array np.all(pos[0] >= 0 and pos[0] < a.shape[0]) and np.all(pos[1] >= 0 and pos[1] < a.shape[1])
-            return (np.all(0 <= X) and np.all(X < self.width) and
-                    np.all(0 <= Y) and np.all(Y < self.height))
-            #return True if 0 <= pos[0] < self.state.shape[0] and 0 <= pos[1] < self.state.shape[1] else False
+        self.paddle1.update(ACTIONS[action])
+        self.paddle2.update(ACTIONS[action]) # TODO: Describe policy for Paddle2 (Bot)
+        self.ball.update()
 
-        paddle1_next_X = self.paddle1.X + actions[action][0]
-        paddle1_next_Y = self.paddle1.Y + actions[action][1]
-        paddle2_next_X = self.paddle2.X + actions[action][0]
-        paddle2_next_Y = self.paddle2.Y + actions[action][1]
-
-        if not in_grid(paddle1_next_X, paddle1_next_Y): # any of the edges outside the grid
-            return self.state, reward, done
-
-        # TODO: update ball position
-        ball_next_X = self.ball.X + self.ball.dir[0]
-        ball_next_Y = self.ball.Y + self.ball.dir[1]
-
-        # If ball coincides in X and Y
-        if np.any(ball_next_X < 0) or np.any(ball_next_X > self.state.shape[0]): # It means Goal
-            reward = -1 if ball_next_X < 0 else 1
+        print("Ball array", self.ball.X)
+        if np.any(self.ball.X < 0) or np.any(self.ball.X > WIDTH): # It means Goal
+            reward = -1 if np.any(self.ball.X < 0) else 1
+            self.score += np.array([0, 1] if np.any(self.ball.X < 0) else [1, 0]) # Update score
+            self.ball.reset(), self.paddle1.reset(), self.paddle2.reset() # Reset to init positions
             return self.state, reward, True
 
         if (
-                not in_grid(ball_next_X, ball_next_Y) # Outside thre grid
-                or np.any(np.isin(ball_next_X, paddle1_next_X)) and np.any(np.isin(ball_next_Y, paddle1_next_Y)) # Hits paddle1
-                or np.any(np.isin(ball_next_X, paddle2_next_X)) and np.any(np.isin(ball_next_Y, paddle2_next_Y)) # Hits paddle2
+                np.any(self.ball.X <= 0) or np.any(self.ball.X >= HEIGHT)
+                or np.any(self.ball.Y <= 0) or np.any(self.ball.Y >= HEIGHT)# Outside the grid
+                or np.any(np.isin(self.ball.X, self.paddle1.X)) and np.any(np.isin(self.ball.Y, self.paddle1.Y)) # Hits paddle1
+                or np.any(np.isin(self.ball.X, self.paddle2.X)) and np.any(np.isin(self.ball.Y, self.paddle2.Y)) # Hits paddle2
         ):
-            self.ball.dir = -self.ball.dir
-            self.ball.pos = self.ball.pos + self.ball.dir
+            print("Entra")
+            self.ball.dir = -self.ball.dir # Reflect direction
 
-        # Update paddle positions
-        self.paddle1.X, self.paddle1.Y  = paddle1_next_X, paddle1_next_Y
-        self.paddle2.X, self.paddle2.Y  = paddle2_next_X, paddle2_next_Y
-
+        self.update_state() # Update state with the new object locations
+        self.render()
         return self.state, reward, done
 
     def reset(self):
-        ...
+        if self.window is not None:
+            pygame.quit()
 
     def render(self):
+        if self.window is None:
+            pygame.init()
+            pygame.display.init()
+            self.window = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
+            pygame.display.set_caption("Pong")
 
-        import matplotlib.pyplot as plt
-        from PIL import Image
+        # TODO: Handle resizable window, whether by a scaling factor on the objects or .transform window ish
+        font = pygame.font.Font('freesansbold.ttf', 20)  # Use default font, size 36
+        clock = pygame.time.Clock()
+        background = pygame.Surface((WIDTH, HEIGHT))
+        background.fill(COLOR_TO_RGB["black"])
 
-        arr = np.zeros((self.state.shape[0], self.state.shape[1], 3), dtype=np.uint8)
-        arr[:, :] = COLOR_TO_RGB["black"] # Init all grid black
+        # Clear screen
+        self.window.blit(background, (0, 0))
+        # Display objects
+        self.display_objects()
+        # Render score
+        score_text = font.render(f"Score: {self.score[0]} - {self.score[1]}", True, COLOR_TO_RGB["white"])
+        self.window.blit(score_text, (20, 20))  # Draw score at top-left
 
-        for obj in [self.paddle1, self.paddle2, self.ball]:
-            arr[obj.X, obj.Y] = COLOR_TO_RGB[obj.color]
+        pygame.display.flip()
+        clock.tick(30)
 
-        import pygame
-
-        # Initialize Pygame
-        pygame.init()
-
-        # Create a window
-        screen = pygame.display.set_mode((self.width, self.height))
-        surface = pygame.surfarray.make_surface(arr)
-        running = True
-        while running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
-
-            # Draw the image on the screen
-            screen.blit(surface, (0, 0))
-
-            # Update the display
-            pygame.display.flip()
-
-        # Quit Pygame
-        pygame.quit()
-
-        """
-        print(arr)
-        img = Image.fromarray(arr)
-        fig, ax = plt.subplots(figsize=(12, 12))
-        ax.imshow(img)
-        ax.axis('off')  # Hide axes
-
-        # Show the figure
-        plt.show()
-        """
-        return True
 
 
